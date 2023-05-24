96c0ea6247f90207b39ee63df8d62b5ea057a8f3
Jaemin-Park <123621015+Park-Jaemin@users.noreply.github.com>
2023-05-24 16:16:10 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 736 ms, Memory: 93308 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
@@ -8,14 +8,14 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
-        Map<String, Integer> map = new HashMap<>();
-        List<String> list = new ArrayList<>();
         int n = Integer.parseInt(st.nextToken());
+        Map<String, Integer> map = new HashMap<>();
+        String[] arr = new String[n + 1];
         int index = 1;
         for (int i = 0; i < n; i++) {
             String name = br.readLine();
             map.put(name, index++);
-            list.add(name);
+            arr[i+1] = name;
         }
 
         n = Integer.parseInt(st.nextToken());
@@ -23,7 +23,7 @@ public class Main {
         for (int i = 0; i < n; i++) {
             String line = br.readLine();
             try {
-                builder.append(list.get(Integer.parseInt(line)-1));
+                builder.append(arr[Integer.parseInt(line)]);
             } catch (NumberFormatException e) {
                 builder.append(map.get(line));
             } finally {
