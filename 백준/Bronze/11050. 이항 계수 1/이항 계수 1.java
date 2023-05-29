d79e8af35ef2938ce4f695b092e695b8f593797f
Jaemin-Park <123621015+Park-Jaemin@users.noreply.github.com>
2023-05-29 16:04:11 +0900

    [Bronze I] Title: 이항 계수 1, Time: 128 ms, Memory: 14256 KB -BaekjoonHub

---
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        System.out.println(factorial(n) / (factorial(n - m) * factorial(m)));
+    }
+
+    private static int factorial(int n) {
+        if (n <= 1) return 1;
+        return n * factorial(n - 1);
+    }
+}
