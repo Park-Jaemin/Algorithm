b98feeb32499f8669d8fa386c5a4a0485b1f49b8
Jaemin-Park <123621015+Park-Jaemin@users.noreply.github.com>
2023-05-30 12:45:58 +0900

    [unrated] Title: 숫자 변환하기, Time: 1.44 ms, Memory: 76.9 MB -BaekjoonHub

--- 프로그래머스/unrated/154538. 숫자 변환하기/숫자 변환하기.java
+++ 프로그래머스/unrated/154538. 숫자 변환하기/숫자 변환하기.java
@@ -1,51 +1,32 @@
 import java.util.*;
 
 class Solution {
-	//x에 적절한 연산을 취한 횟수와 그 결과를 저장할 Class
-    static class Node{
-        int num;
-        int count;
-        Node(int num, int count){
-            this.num = num;
-            this.count = count;
-        }
-    }
-    static Queue<Node> q;
-    static boolean[] visited;
-    static int limit = 1000000;
     public int solution(int x, int y, int n) {
-        int answer = 0;
-        if(x == y)
-            return 0;
-        q = new LinkedList<>();
-        
-        //제일 큰 수가 나오는게 3을 곱하는 것이고 각 인덱스를 숫자 그대로 보기위해서 +1
-        visited = new boolean[3*limit+1];
-        q.offer(new Node(x, 0));
-        
-        while(!q.isEmpty()){
-            Node cur = q.poll();
-            int curNum = cur.num;
-            int curCount = cur.count;
-            if(curNum == y){
-                answer = curCount;
-                break;
+        boolean[] visit = new boolean[y+1];
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[]{x, 0});
+        visit[x] = true;
+
+        while (!queue.isEmpty()) {
+            int[] poll = queue.poll();
+            int cur = poll[0];
+            int count = poll[1];
+
+            if (cur == y) return count;
+
+            if (cur+n <= y && !visit[cur+n]) {
+                visit[cur+n] = true;
+                queue.offer(new int[]{cur+n, count+1});
             }
-            //각 연산을 취했을 때 나온적이 없는 수인지(!visited)
-            //그리고 그 수가 제한조건을 넘어가지 않는지
-            if(!visited[curNum+n] && curNum+n <= limit){
-                visited[curNum+n] = true;
-                q.offer(new Node(curNum+n, curCount+1));
+            if (cur*2 <= y && !visit[cur*2]) {
+                visit[cur*2] = true;
+                queue.offer(new int[]{cur*2, count+1});
             }
-            if(!visited[curNum*2] && curNum*2 <= limit){
-                visited[curNum*2] = true;
-                q.offer(new Node(curNum*2, curCount+1));
-            }
-            if(!visited[curNum*3] && curNum*3 <= limit){
-                visited[curNum*3] = true;
-                q.offer(new Node(curNum*3, curCount+1));
+            if (cur*3 <= y && !visit[cur*3]) {
+                visit[cur*3] = true;
+                queue.offer(new int[]{cur*3, count+1});
             }
         }
-        return answer == 0 ? -1 : answer;
+        return -1;
     }
 }
