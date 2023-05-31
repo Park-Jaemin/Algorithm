b8488ce3849c6326e7f3f0d497a2baed25af0fc4
Jaemin-Park <123621015+Park-Jaemin@users.noreply.github.com>
2023-05-31 11:46:43 +0900

    [Silver II] Title: 스택 수열, Time: 384 ms, Memory: 27620 KB -BaekjoonHub

---
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        Stack<Integer> stack = new Stack<>();
+        int count = Integer.parseInt(br.readLine());
+        int start = 0;
+        for (int i = 0; i < count; i++) {
+            int value = Integer.parseInt(br.readLine());
+            if (value > start) {
+                for(int j = start + 1; j <= value; j++) {
+                    stack.push(j);
+                    sb.append("+").append("\n");
+                }
+                start = value;
+            } else if (stack.peek() != value) {
+                System.out.println("NO");
+                return;
+            }
+
+            stack.pop();
+            sb.append("-").append("\n");
+        }
+        System.out.println(sb);
+    }
+}
