9b8393a4c61cd9b8802c340a957fdce72c2f10b6
Jaemin-Park <123621015+Park-Jaemin@users.noreply.github.com>
2023-06-02 19:12:46 +0900

    [Bronze V] Title: 팩토리얼 2, Time: 128 ms, Memory: 14084 KB -BaekjoonHub

---
+++ 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        long num = factorial(n);
+        System.out.println(num);
+    }
+
+    private static long factorial(int n) {
+        if (n <= 1) {
+            return 1;
+        }
+        return n * factorial(n-1);
+    }
+}
