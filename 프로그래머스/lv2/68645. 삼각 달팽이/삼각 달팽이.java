3f9bc7e7e35fd49877a4c451a374881da322a826
Jaemin-Park <123621015+Park-Jaemin@users.noreply.github.com>
2023-06-12 20:50:36 +0900

    [level 2] Title: 삼각 달팽이, Time: 15.81 ms, Memory: 144 MB -BaekjoonHub

--- 프로그래머스/lv2/68645. 삼각 달팽이/삼각 달팽이.java
+++ 프로그래머스/lv2/68645. 삼각 달팽이/삼각 달팽이.java
@@ -5,24 +5,39 @@ class Solution {
         int x = -1; // x 좌표
         int y = 0; // y 좌표
         int val = 1; // 달팽이 채우기 값
-
+        
+        
+        
+        // 0,0 / 0,1 / 0,2 / 0,3
+        // 1,1 / 1,2 / 1,3
+        // 2,2 / 2,3
+        // 3,3
+            
         for(int i = 0; i < n; i++) {
-            for (int j = i; j < n; j++) {
-                if (i % 3 == 0) {
+            for (int j = i; j < n; j++) { 
+                if (i % 3 == 0) { // 세로
                     x++;
-                } else if (i % 3 == 1) {
+                } else if (i % 3 == 1) { // 가로
                     y++;
                 } else {
                     x--;
                     y--;
                 }
-                arr[x][y] = val++;
+                arr[x][y] = val++; // 11
             }
         }
+        
+        //?000
+        //??nn
+        //???n
+        //????
 
-        int[] answer = new int[val - 1];
+        // 0, 0
+        // 1, 0 / 1,1
+        // 2, 0 / 2,1 / 2,2
+        int[] answer = new int[val - 1]; // 10
         val = 0;
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) { 
             for (int j = 0; j <= i; j++) {
                 answer[val++] = arr[i][j];
             }
