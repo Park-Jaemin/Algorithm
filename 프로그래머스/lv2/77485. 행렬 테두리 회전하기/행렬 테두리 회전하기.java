13c99e415d61dab3907482be5ec6bf5188b7adde
Jaemin-Park <123621015+Park-Jaemin@users.noreply.github.com>
2023-06-20 17:16:39 +0900

    [level 2] Title: 행렬 테두리 회전하기, Time: 4.98 ms, Memory: 97.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/77485. 행렬 테두리 회전하기/행렬 테두리 회전하기.java
@@ -0,0 +1,53 @@
+class Solution {
+    
+    static int[][] map;
+    static int[] answer;
+    
+    public int[] solution(int rows, int columns, int[][] queries) {
+        initMap(rows, columns);
+        answer = new int[queries.length];
+
+        for (int i = 0; i < queries.length; i++) {
+            answer[i] = rotation(queries[i]);
+        }
+
+        return answer;
+    }
+    
+    void initMap(int rows, int columns) {
+        map = new int[rows][columns];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                map[i][j] = i * map[i].length + j + 1;
+            }
+        }
+    }
+    
+    int rotation(int[] rotationInfo) {
+        int min = 100*100+1;
+
+        int x1 = rotationInfo[0]-1;
+        int x2 = rotationInfo[2]-1;
+        int y1 = rotationInfo[1]-1;
+        int y2 = rotationInfo[3]-1;
+
+        int start = map[x1][y1];
+
+        for (int i = x1; i < x2; i++) {
+            min = Math.min(min, map[i][y1] = map[i+1][y1]);
+        }
+        for (int i = y1; i < y2; i++) {
+            min = Math.min(min, map[x2][i] = map[x2][i+1]);
+        }
+        for (int i = x2; i > x1; i--) {
+            min = Math.min(min, map[i][y2] = map[i-1][y2]);
+        }
+        for (int i = y2; i > y1; i--) {
+            min = Math.min(min, map[x1][i] = map[x1][i-1]);
+        }
+
+        min = Math.min(min, map[x1][y1+1] = start);
+
+        return min;
+    }
+}
