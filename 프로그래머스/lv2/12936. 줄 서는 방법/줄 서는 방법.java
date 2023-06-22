616bc459de74403199843d6227cf90521a40a149
Jaemin-Park <123621015+Park-Jaemin@users.noreply.github.com>
2023-06-22 15:58:28 +0900

    [level 2] Title: 줄 서는 방법, Time: 0.05 ms, Memory: 52.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12936. 줄 서는 방법/줄 서는 방법.java
@@ -0,0 +1,25 @@
+
+import java.util.*;
+
+class Solution {
+    public int[] solution(int n, long k) {
+        List<Integer> people = new ArrayList<>();
+        int[] answer = new int[n];
+
+        long totalCaseCount = 1;
+        for (int i = 1; i <= n; i++) {
+            people.add(i);
+            totalCaseCount *= i;
+        }
+
+        k--;
+        int index = 0;
+        while (index < n) {
+            totalCaseCount /= n-index;
+            answer[index++] = people.remove((int) (k / totalCaseCount));
+            k %= totalCaseCount;
+        }
+
+        return answer;
+    }
+}
