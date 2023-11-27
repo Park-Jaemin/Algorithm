ec4db3c9e1011669cde739a3e587a0b4bcadc371
Park Jae-Min <123621015+Park-Jaemin@users.noreply.github.com>
2023-11-27 17:57:05 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.22 ms, Memory: 69.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String s) {
+        StringBuilder sb = new StringBuilder();
+        String[] split = s.toLowerCase().split("");
+        boolean first = true;
+        
+        
+        for (int i = 0; i < split.length; i++) {
+            String word = split[i];
+            if (word.equals(" ")) {
+                first = true;
+                sb.append(word);
+                continue;
+            }
+            
+            if (first) {
+                word = word.toUpperCase();
+                first = false;
+            }
+            
+            sb.append(word);
+        }
+        
+        return sb.toString();
+    }
+}
