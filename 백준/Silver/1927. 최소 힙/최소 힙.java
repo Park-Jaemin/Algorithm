06901c921065bf34a6c1b282b24b3b33cc6bc6fa
Park Jae-Min <123621015+Park-Jaemin@users.noreply.github.com>
2023-11-27 18:22:05 +0900

    [Silver II] Title: 최소 힙, Time: 336 ms, Memory: 26420 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
+
+        int size = Integer.parseInt(br.readLine());
+        while (size-- > 0) {
+            int num = Integer.parseInt(br.readLine());
+            if (num == 0) {
+                sb.append(queue.isEmpty() ? 0 : queue.poll());
+                if (size > 0) sb.append("\n");
+            } else {
+                queue.offer(num);
+            }
+        }
+        System.out.println(sb);
+    }
+}
