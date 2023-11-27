17550903a0a26abfcfddf44af6ce69e015852188
Park Jae-Min <123621015+Park-Jaemin@users.noreply.github.com>
2023-11-27 17:27:51 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 9.33 ms, Memory: 77.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String s) {
+        StringTokenizer st = new StringTokenizer(s);
+        
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        
+        while (st.hasMoreElements()) {
+            int num = Integer.parseInt(st.nextToken());
+            
+            min = Math.min(min, num);
+            max = Math.max(max, num);
+        }
+        
+        
+        
+        return min + " " + max;
+    }
+}
