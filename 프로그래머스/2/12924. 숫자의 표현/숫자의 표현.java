297fa57d3f84944a509e1c412fd156b5c7f9d905
Park Jae-Min <123621015+Park-Jaemin@users.noreply.github.com>
2023-11-29 15:34:43 +0900

    [level 2] Title: 숫자의 표현, Time: 1.43 ms, Memory: 51.8 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
@@ -1,26 +1,23 @@
 class Solution {
     public int solution(int target) {
         int answer = 0;
-        
-        for (int i = 1; i <= target; i++) {
-            answer += calc(i, target, 0, 0);   
+
+        for(int i = 1; i<=target; i++){
+            int sum = 0;
+            
+            for(int j = i; j<=target; j++){
+                sum += j;
+                
+                if (sum == target) {
+                    answer++;
+                    break;
+                }
+                if(sum > target) {
+                    break;
+                }
+            }
         }
-        
+
         return answer;
-    }
-    
-    int calc(int now, int target, int sum, int count) {
-        sum += now;
-        
-        if (sum > target) {
-            return count;
-        }
-        
-        if (sum == target) {
-            count++;
-        }
-        
-        count = calc(now+1, target, sum, count);
-        return count;
     }
 }
