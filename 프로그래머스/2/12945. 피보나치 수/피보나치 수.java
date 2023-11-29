981c9f5497cc652b0eb2a47541d812e585d148e3
Park Jae-Min <123621015+Park-Jaemin@users.noreply.github.com>
2023-11-29 16:26:31 +0900

    [level 2] Title: 피보나치 수, Time: 4.29 ms, Memory: 76.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -0,0 +1,22 @@
+class Solution {
+//     public int solution(int n) {
+//         return fibonacci(n) % 1234567;
+//     }
+    
+//     int fibonacci(int n) {
+//         if (n == 0) return 0;
+//         if (n == 1) return 1;
+//         return fibonacci(n-2) + fibonacci(n-1);
+//     }
+    
+    public long solution(int n) {
+        long[] arr = new long[n+1];
+        arr[0] = 0;
+        arr[1] = 1;
+        
+        for (int i = 2; i < n+1; i++) {
+            arr[i] = arr[i-2] % 1234567 + arr[i-1] % 1234567;
+        }
+        return arr[n] % 1234567;
+    }
+}
