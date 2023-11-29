8596f39c07de71b9ca92e6e40110db31c6ecd276
Park Jae-Min <123621015+Park-Jaemin@users.noreply.github.com>
2023-11-29 20:43:00 +0900

    [level 2] Title: 올바른 괄호, Time: 18.98 ms, Memory: 52.9 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -2,19 +2,41 @@ import java.util.*;
 
 class Solution {
     boolean solution(String s) {
-        int count = 0;
-        
+        Stack<Character> stack = new Stack<>();
+
         for (int i = 0; i < s.length(); i++) {
             char bracket = s.charAt(i);
-            
-            if (bracket == '(') count++;
-            else count--;
-            
-            if (count < 0) {
-                return false;
+
+            if (bracket == '(') {
+                stack.push(bracket);
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                stack.pop();
             }
         }
+
+        if (!stack.isEmpty()) {
+            return false;
+        }
+
+        return true;
         
-        return count > 0 ? false : true;
+        
+//         int count = 0;
+        
+//         for (int i = 0; i < s.length(); i++) {
+//             char bracket = s.charAt(i);
+            
+//             if (bracket == '(') count++;
+//             else count--;
+            
+//             if (count < 0) {
+//                 return false;
+//             }
+//         }
+        
+//         return count > 0 ? false : true;
     }
 }
