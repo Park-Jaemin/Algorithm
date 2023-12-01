105e8f646ae4d9cf06754da5cc9797a5a6dc1864
Park Jae-Min <123621015+Park-Jaemin@users.noreply.github.com>
2023-12-01 22:08:34 +0900

    [level 2] Title: 카펫, Time: 0.03 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -3,33 +3,17 @@ class Solution {
         int total = brown + yellow;
         int col = 3;
         int row = total;
+        
         for (; col <= row; col++) {
             if (total % col == 0) {
                 row = total / col;
-            }
-
-            if ((col - 2) * (row - 2) == yellow) {
-                return new int[]{row, col};
+                
+                if ((col - 2) * (row - 2) == yellow) {
+                    return new int[]{row, col};
+                }
             }
         }
 
         return null;
     }
 }
-
-/*
-    row = 가로
-    col = 세로
-    
-    row >= col
-    row * col = brown + yellow
-    row * col - ((row-2) * (col-2)) = brown
-    row * col - ((row-2) * (col-2))
-    (row-2) * (col-2) = yellow
-    row * col = brown + ((row-2) * (col*2))
-    
-    row * col - ((row-2) * (col-2)) - brown = 0
-    
-    4 * 3 = 10 + ((4-2) * (3-2))
-    12 = 10 + 2
-*/
