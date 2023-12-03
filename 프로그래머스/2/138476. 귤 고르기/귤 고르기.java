9f58a0276f6fe0a48aa87e20008088b53631cc66
Park Jae-Min <123621015+Park-Jaemin@users.noreply.github.com>
2023-12-03 15:40:24 +0900

    [level 2] Title: 귤 고르기, Time: 117.77 ms, Memory: 108 MB -BaekjoonHub

---
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int k, int[] tangerine) {
+        int answer = 0;
+        
+        Arrays.sort(tangerine);
+        int limit = tangerine.length;
+        int[] count = new int[tangerine[limit-1]+1];
+        
+        for (int i = 0; i < limit; i++) {
+            count[tangerine[i]-1]++;
+        }
+        
+        Arrays.sort(count);
+        int sum = 0;
+        for (int i = count.length-1; i >= 0 && sum < k; i--) {
+            sum += count[i];
+            answer++;
+        }
+        
+        return answer;
+    }
+}
