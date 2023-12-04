acf4e4de2149933728680de12feac833833f8c64
Park Jae-Min <123621015+Park-Jaemin@users.noreply.github.com>
2023-12-04 18:24:51 +0900

    [level 2] Title: 괄호 회전하기, Time: 0.38 ms, Memory: 76.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String s) {
+        int answer = 0;
+        
+        char[] arr = s.toCharArray();
+        Deque<Character> deque = new LinkedList<>();
+        
+        for (char bracket : arr) {
+            deque.offer(bracket);
+        }
+        
+        for (int i = 0; i < arr.length; i++) {
+            answer += checkBracket(deque.iterator()) ? 1 : 0;
+            deque.offerLast(deque.pollFirst());
+        }
+        
+        return answer;
+    }
+    
+    boolean checkBracket(Iterator<Character> iterator) {
+        Stack<Character> stack = new Stack<>();
+        while (iterator.hasNext()) {
+            char bracket = iterator.next();
+            if (stack.isEmpty() && (bracket == ')' || bracket == ']' || bracket == '}')) {
+                return false;
+            }
+            
+            if (!stack.isEmpty() && (stack.peek()+2 == bracket || stack.peek()+1 == bracket)) {
+                stack.pop();
+            } else {
+                stack.push(bracket);
+            }
+        }
+        
+        return stack.isEmpty();
+    }
+}
