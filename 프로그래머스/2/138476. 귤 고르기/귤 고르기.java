6fba7354f37f897e68f31ae39973206cad0dc6bb
Park Jae-Min <123621015+Park-Jaemin@users.noreply.github.com>
2023-12-06 22:56:36 +0900

    [level 2] Title: 귤 고르기, Time: 84.16 ms, Memory: 116 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
@@ -4,22 +4,22 @@ import java.util.stream.Collectors;
 class Solution {
     public int solution(int k, int[] tangerine) {
         int answer = 0;
-        
-        Map<Integer, Integer> map = new TreeMap<>();
-        for (int key : tangerine) {
-            map.put(key, map.getOrDefault(key, 0) + 1);
-        }
-        
-        List<Integer> list = new ArrayList<>(map.keySet());
-        list.sort((o1, o2) -> map.get(o2) - map.get(o1));
 
-        for (Integer key : list) {
-            k -= map.get(key);
-            answer++;
-            
-            if (k <= 0) break;
+        Arrays.sort(tangerine);
+        int limit = tangerine.length;
+        int[] count = new int[tangerine[limit-1]+1];
+
+        for (int i = 0; i < limit; i++) {
+            count[tangerine[i]-1]++;
         }
-        
+
+        Arrays.sort(count);
+        int sum = 0;
+        for (int i = count.length-1; i >= 0 && sum < k; i--) {
+            sum += count[i];
+            answer++;
+        }
+
         return answer;
     }
 }
