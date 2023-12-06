cff86572dc7776eb08baddb85ab54d3ca98b52d6
Park Jae-Min <123621015+Park-Jaemin@users.noreply.github.com>
2023-12-06 22:38:29 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.74 ms, Memory: 79 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -1,15 +1,30 @@
 class Solution {
+    Long[] arr;
     public long solution(int n) {
-        long[] arr = new long[n];
-        arr[0] = 1;
-        if (n > 1) {
-            arr[1] = 2;
-        }
+        // DP bottom up
+//         long[] arr = new long[n];
+//         arr[0] = 1;
+//         if (n > 1) {
+//             arr[1] = 2;
+//         }
         
-        for (int i = 2; i < n; i++) {
-            arr[i] = (arr[i-2] + arr[i-1]) % 1234567;
-        }
+//         for (int i = 2; i < n; i++) {
+//             arr[i] = (arr[i-2] + arr[i-1]) % 1234567;
+//         }
         
-        return arr[n-1];
+//         return arr[n-1];
+        arr = new Long[n+1];
+        return dp(n);
+    }
+    
+    long dp(int n) {
+        if (n == 1) return arr[1] = 1L;
+        if (n == 2) return arr[2] = 2L;
+        
+        if (arr[n] == null) {
+            return arr[n] = (dp(n-2) + dp(n-1)) % 1234567;
+        } else {
+            return arr[n];
+        }
     }
 }
