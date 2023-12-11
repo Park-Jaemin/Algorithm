2baaf398ffd06b49ebeea38241efc90621fee61f
Park Jae-Min <123621015+Park-Jaemin@users.noreply.github.com>
2023-12-11 20:01:06 +0900

    [level 2] Title: 프로세스, Time: 0.67 ms, Memory: 75 MB -BaekjoonHub

---
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] priorities, int location) {
+        int answer = 0;
+        
+        Queue<Integer> queue = new PriorityQueue<>(Collections.reverseOrder());
+        Deque<int[]> deque = new LinkedList<>();
+        
+        for (int i = 0; i < priorities.length; i++) {
+            queue.offer(priorities[i]);
+            deque.offerLast(new int[]{priorities[i], i});
+        }
+        
+        while (true) {
+            int[] poll = deque.poll();
+            int priority = poll[0];
+            int index = poll[1];
+            
+            if (queue.peek() == priority) {
+                queue.poll();
+                answer++;
+                if (index == location) break;
+            } else {
+                deque.offerLast(poll);
+            }
+        }
+        
+        return answer;
+    }
+}
