a216ba42afa86c0537ee0472cbe092aaeecf6c99
Park Jae-Min <123621015+Park-Jaemin@users.noreply.github.com>
2023-12-13 03:00:10 +0900

    [level 2] Title: 전화번호 목록, Time: 234.98 ms, Memory: 132 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -2,41 +2,18 @@ import java.util.*;
 
 class Solution {
     public boolean solution(String[] phone_book) {
-//         Set<String> set = new HashSet<>();
+        Set<String> set = new HashSet<>();
         
-//         for (int i = 0; i < phone_book.length; i++) {
-//             String number = phone_book[i];
-            
-//             if (i == 0) {
-//                 set.add(number);
-//                 continue;
-//             }
-            
-//             for (int j = 0; j < number.length(); j++) {
-//                 if (set.contains(number.substring(0, j))) return false;
-//             }
-//             set.add(number);
-//         }
-        
-//         return true;
-        
-        Map<String, Integer> map = new HashMap<>();
-
-        // 모든 phone 번호 hashMap에 담기
-        for (int i = 0; i < phone_book.length; i++) {
-            map.put(phone_book[i], i);
+        for (String number : phone_book) {
+            set.add(number);
         }
-
-        for (int i = 0; i < phone_book.length; i++) {
-            // 전화번호의 길이만큼 순회
-            for (int j = 0; j < phone_book[i].length(); j++) {
-                // 해당 전화번호의 접두어가 key로 가진 경우
-                if (map.containsKey(phone_book[i].substring(0,j))) {
-                    return false;
-                }
+        
+        for (String number : phone_book) {
+            for (int i = 0; i < number.length(); i++) {
+                if (set.contains(number.substring(0,i))) return false;
             }
         }
-
+        
         return true;
     }
 }
