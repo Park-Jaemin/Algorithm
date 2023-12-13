261f13de45df1e79cac746e8a61fcfba61804ac7
Park Jae-Min <123621015+Park-Jaemin@users.noreply.github.com>
2023-12-13 13:21:45 +0900

    [level 2] Title: 타겟 넘버, Time: 0.31 ms, Memory: 78.7 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -5,11 +5,9 @@ class Solution {
         return dfs(numbers, target, 0, 0);
     }
     
-    int dfs(int[] numbers, int target, int depth, int now) {
-        if (depth == numbers.length) {
-            return now == target ? 1 : 0;
-        }
+    int dfs(int[] numbers, int target, int now, int depth) {
+        if (depth == numbers.length) return now == target ? 1 : 0;
         
-        return dfs(numbers, target, depth+1, now+numbers[depth]) + dfs(numbers, target, depth+1, now-numbers[depth]);
+        return dfs(numbers, target, now+numbers[depth], depth+1) + dfs(numbers, target, now-numbers[depth], depth+1);
     }
 }
