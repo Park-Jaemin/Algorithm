ba64adfce158b979e1e125bbaebb04754205f833
Park Jae-Min <123621015+Park-Jaemin@users.noreply.github.com>
2023-12-14 22:32:27 +0900

    [level 2] Title: 더 맵게, Time: 1633.93 ms, Memory: 123 MB -BaekjoonHub

---
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] scoville, int K) {
+        int answer = 0;
+        
+        Queue<Integer> queue = new PriorityQueue<>();
+        for (int sco : scoville) {
+            queue.offer(sco);
+        }
+        
+        while (queue.peek() < K) {
+            
+            if (queue.size() == 1) return -1;
+
+            int sco1 = queue.poll();
+            int sco2 = queue.poll() * 2;
+            
+            queue.offer(sco1 + sco2);
+        }
+        
+        return scoville.length - queue.size();
+    }
+}
