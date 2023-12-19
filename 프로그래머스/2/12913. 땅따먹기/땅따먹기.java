509342ed9dfb256ed0b3ffa3785b65f7085db8c9
Park Jae-Min <123621015+Park-Jaemin@users.noreply.github.com>
2023-12-19 19:43:28 +0900

    [level 2] Title: 땅따먹기, Time: 19.49 ms, Memory: 86 MB -BaekjoonHub

--- 프로그래머스/2/12913. 땅따먹기/땅따먹기.java
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.java
@@ -1,7 +1,7 @@
 class Solution {
     int solution(int[][] land) {
         int answer = 0;
-
+        
         for (int i = 1; i < land.length; i++) {
             land[i][0] += Math.max(Math.max(land[i-1][1], land[i-1][2]), land[i-1][3]);
             land[i][1] += Math.max(Math.max(land[i-1][0], land[i-1][2]), land[i-1][3]);
@@ -12,7 +12,7 @@ class Solution {
         for (int sum : land[land.length - 1]) {
             answer = Math.max(answer, sum);
         }
-
+        
         return answer;
     }
 }
