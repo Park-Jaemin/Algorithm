5b8568a60d7e99daca5248f993ee90e3107c3545
Park Jae-Min <123621015+Park-Jaemin@users.noreply.github.com>
2023-12-20 01:58:41 +0900

    [level 2] Title: 롤케이크 자르기, Time: 16.52 ms, Memory: 138 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
@@ -2,31 +2,34 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] topping) {
-        int answer = 0;
-        
-        Map<Integer, Integer> left = new HashMap<>();
-        Map<Integer, Integer> right = new HashMap<>();
+        int[] left = new int[10001];
+        int[] right = new int[10001];
+        int rightTypeCount = 0;
         
         for (int type : topping) {
-            left.put(type, left.getOrDefault(type, 0) + 1);
+            if (right[type] == 0) {
+                rightTypeCount++;
+            }
+            right[type]++;
         }
-        
+
+        int answer = 0;
+        int leftTypeCount = 0;
         for (int type : topping) {
-            right.put(type, right.getOrDefault(type, 0) + 1);
-            
-            if (left.get(type) != null) {
-                left.put(type, left.get(type) - 1);
+            if (right[type] == 1) {
+                rightTypeCount--;
             }
-            
-            if (left.get(type) == 0) {
-                left.remove(type);
+
+            if (left[type] == 0) {
+                leftTypeCount++;
             }
-            
-            if (left.size() == right.size()) {
+            right[type]--;
+            left[type]++;
+
+            if (leftTypeCount == rightTypeCount) {
                 answer++;
             }
         }
-        
         return answer;
     }
 }
