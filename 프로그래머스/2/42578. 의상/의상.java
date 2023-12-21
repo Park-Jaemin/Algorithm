db717f8be1e91ed460daa5944b0f78c617964ad4
Park Jae-Min <123621015+Park-Jaemin@users.noreply.github.com>
2023-12-21 22:11:00 +0900

    [level 2] Title: 의상, Time: 0.05 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -4,7 +4,7 @@ class Solution {
     public int solution(String[][] clothes) {
         Map<String, Integer> map = new HashMap<>();
         for (String[] cloth : clothes) {
-            map.put(cloth[1], map.getOrDefault(cloth[1], 0) + 1);
+            map.put(cloth[1], map.getOrDefault(cloth[1], 0) + 1); 
         }
         
         int sum = 1;
