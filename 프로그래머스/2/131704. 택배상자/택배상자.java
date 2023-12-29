a72fd4a287643150573cd39201bfec942ad22101
Park Jae-Min <123621015+Park-Jaemin@users.noreply.github.com>
2023-12-29 23:25:00 +0900

    [level 2] Title: 택배상자, Time: 53.56 ms, Memory: 147 MB -BaekjoonHub

--- 프로그래머스/2/131704. 택배상자/택배상자.java
+++ 프로그래머스/2/131704. 택배상자/택배상자.java
@@ -1,19 +1,15 @@
 import java.util.*;
 
 class Solution {
-    public int solution(int[] order) {
-        int answer = 0;
-        
+    public int solution(int[] order) { 
         int index = 0;
         Stack<Integer> stack = new Stack<>();
         for (int i = 1; i <= order.length; i++) {
             if (order[index] == i) {
                 index++;
-                answer++;
                 
                 while (!stack.isEmpty() && stack.peek() == order[index]) {
                     index++;
-                    answer++;
                     stack.pop();
                 }
                 
@@ -22,6 +18,6 @@ class Solution {
             }
         }
         
-        return answer;
+        return index;
     }
 }
