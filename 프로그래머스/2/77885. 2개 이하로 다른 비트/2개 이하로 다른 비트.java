62fd7a287ab79b940a98951b1b3107be91b77855
Park Jae-Min <123621015+Park-Jaemin@users.noreply.github.com>
2024-01-02 20:06:22 +0900

    [level 2] Title: 2개 이하로 다른 비트, Time: 219.56 ms, Memory: 127 MB -BaekjoonHub

---
+++ 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.java
@@ -0,0 +1,32 @@
+class Solution {
+    public long[] solution(long[] numbers) {
+        long[] answer = numbers.clone();
+
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = find(answer[i]);
+        }
+
+        return answer;
+    }
+    
+    long find(long number) {
+        StringBuilder curBit = new StringBuilder("0" + Long.toBinaryString(number));
+        int length = curBit.length() - 1;
+
+        if (curBit.charAt(length) == '0') {
+            return number + 1;
+        }
+
+        while (curBit.charAt(length) == '1') {
+            curBit.replace(length, length+1, "0");
+            length--;
+        }
+        curBit.replace(length, length+1, "1");
+
+        for (int i = curBit.length() - 1; i - length >= 2 && i >= 0; i--) {
+            curBit.replace(i, i+1, "1");
+        }
+
+        return Long.parseLong(curBit.toString(), 2);
+    }
+}
